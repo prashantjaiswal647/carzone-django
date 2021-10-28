ad3bc64d6fe020b51f2925a7af28c77762d25b62
prashantjaiswal647 <prashantjaiswal647@gmail.com>
2021-10-28 19:16:36 +0530

    setting up postgres database

--- carzone/settings.py
+++ carzone/settings.py
@@ -77,7 +77,10 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'qwerty',
+        'HOST': 'localhost',
     }
 }
 
