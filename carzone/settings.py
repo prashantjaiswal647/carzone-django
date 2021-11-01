e139003903d0db2ac620c8c67ef728b31d196cbd
prashantjaiswal647 <prashantjaiswal647@gmail.com>
2021-11-01 20:33:57 +0530

    Adding accounts app for register,login,logout and making title dynamic

--- carzone/settings.py
+++ carzone/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'cars.apps.CarsConfig',
     'pages.apps.PagesConfig',
+    'accounts.apps.AccountsConfig',
     'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- carzone/urls.py
+++ carzone/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('cars/', include('cars.urls')),
     path('contacts/', include('contacts.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
